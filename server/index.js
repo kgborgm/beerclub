622073a46ad3c7365f385159f66bff20f85080e8
Katie Caskey <kcaskey@bou-kcaskey-mbp.lan>
2021-06-21 14:48:19 -0400

    update gitignore file; updated styling; created initial table component; using sample chart component

--- server/index.js
+++ server/index.js
@@ -1,16 +1,57 @@
 const path = require("path");
 const express = require("express");
+const csvdb = require("csv-database");
+const cors = require("cors");
+
 const app = express(); // create express app
+app.use(cors());
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+// });
+app.get('/', async(req, res) => {
+	res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
+
+app.get('/api/members', async(req, res) => {
+	try {
+		const db = await csvdb("beerclubdata.csv", ["member", "beer-style", "date"], ",");
+		const members = await db.get();
+		res.send(members);
+	} catch (err) {
+		console.error("Error in getting members", err);
+		res.sendStatus(500);
+	}
+})
+
+app.get('/api/beer-styles/:member', async(req, res) => {
+	try {
+		const db = await csvdb("beerclubdata.csv", ["member", "beer-style", "date"], ",");
+		const results = await db.get({member: req.params.member});
+		res.send(results);
+	} catch (err) {
+		console.error("Error in getting beer styles", err);
+		res.sendStatus(500);
+	}
+})
+
+app.get('/api/consumptions/:member', async (req, res) => {
+	try {
+		const db = await csvdb("beerclubdata.csv", ["member", "beer-style", "date"], ",");
+		const results = await db.get({member: req.params.member});
+		res.send(members);
+	} catch (err) {
+		console.error("Error in getting consumptions", err);
+		res.sendStatus(500);
+	}
+})
 
 // start express server on port 3000
 app.listen(3000, function () {
 	console.log('listening on port 3000!');
 });
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './tavour_logo_1.png';
+import {
+  Container,
+  Row,
+  Col,
+  Table,
+  Navbar
+} from 'react-bootstrap';
+import ExampleChart from './components/PieChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Beer Club Analytics by Tavour
+        </Navbar.Brand>
+      </Navbar>
+      <Container fluid="xl">
+        <Row>
+          <Col>
+            <h1 className="App-header">Data Analysis for Beer Club</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="float-left">Consumptions per Member:</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            
+          </Col>
+          <Col>
+            <ExampleChart />
+          </Col>
+        </Row>
+
+      </Container>
     </div>
   );
 }
